c1da878326fa77b9007c99c1efc5e1a64fa507f0
sathya_mallow <sathyamoorthy@mallow-tech.com>
2023-11-14 14:27:48 +0530

    hey starting the projet

---
+++ script.js
@@ -0,0 +1,7 @@
+const invoke = document.getElementById("head");
+
+const fire_the_function = () => {
+  console.log("Hey u clicked the div tag");
+};
+
+invoke.addEventListener("click", fire_the_function);
